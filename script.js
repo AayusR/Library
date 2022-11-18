de75aa1662ec95f870e6fa16b020575ca61ad71a
AayusR <regmiaayush069@gmail.com>
2022-11-18 13:10:20 +0545

    Add functions in script.js

--- script.js
+++ script.js
@@ -1,19 +1,107 @@
 
+showBooks();
 const addButton = document.querySelector('.add-btn')
 const formContainer = document.querySelector('.form')
 const submitButton = document.querySelector('.submit-btn')
-const form = document.querySelector('form')
 const closeButton = document.querySelector('.close-btn')
 
-addButton.addEventListener('click',()=>{
+const form = document.querySelector('form')
+const updateButton = document.querySelectorAll('.update')
+const removeButtons = document.querySelectorAll('.remove')
+
+// functions
+function Book(title, author, pages, status) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.status = status
+}
+
+function addBookToLibrary() {
+    const title = document.querySelector('#title').value
+    const author = document.querySelector('#author').value
+    const pages = document.querySelector('#pages').value
+    const readYes = document.getElementById('yes')
+    const readNo = document.getElementById('no')
+    let status;
+    if(readYes.checked)
+        status = 'read';
+    else if(readNo.checked)
+        status = 'not read'
+    const newBook = new Book(title, author, pages, status)
+
+    let books = localStorage.getItem('books')
+
+    if (books == null)
+        myLibrary = [];
+    else
+        myLibrary = JSON.parse(books);
+
+    myLibrary.push(newBook);
+    localStorage.setItem('books', JSON.stringify(myLibrary));
+    showBooks();
+}
+
+function showBooks() {
+    let books = localStorage.getItem('books')
+    if (books == null)
+        myLibrary = [];
+    else
+        myLibrary = JSON.parse(books);
+    let html = '';
+    myLibrary.forEach(function (book, index) {
+        html += `
+        <div class="card">
+            <p>${book.title}</p>
+            <p>${book.author}</p>
+            <p>${book.pages}</p>
+            <button class="update btn" data-status=${index} onclick='changeStatus(this.dataset.status)' >${book.status}</button>
+            <button id='${index}' class="remove btn" onclick='removeBook(this.id)' >Remove</button>
+        </div>
+        `
+        document.getElementById('card-container').innerHTML = html;
+    })
+    
+}
+
+function removeBook(index){
+    console.log('removing book', index)
+
+    let books = localStorage.getItem('books')
+    if (books == null)
+        myLibrary = [];
+    else
+        myLibrary = JSON.parse(books);
+
+    myLibrary.splice(index,1)
+    localStorage.setItem('books', JSON.stringify(myLibrary));
+    showBooks(); 
+}
+
+function changeStatus(index){
+    let books = localStorage.getItem('books')
+    if (books == null)
+        myLibrary = [];
+    else
+        myLibrary = JSON.parse(books);
+    console.log(myLibrary[index].status)
+    let previousStatus = myLibrary[index].status;
+    let newStatus;
+    if(previousStatus === 'read')
+        newStatus = 'not read'
+    else
+        newStatus = 'read'
+    myLibrary[index].status = newStatus
+    localStorage.setItem('books', JSON.stringify(myLibrary));
+    showBooks();
+}
+
+//event listener and actions
+addButton.addEventListener('click', () => {
     formContainer.style.display = 'flex';
 })
 
-form.addEventListener('submit', ()=>{
+form.addEventListener('submit', () => {
+    addBookToLibrary();
     formContainer.style.display = 'none';
 })
-
-closeButton.addEventListener('click', ()=>{
-    formContainer.style.display = 'none';
-})
-
